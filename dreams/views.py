20932eb79b12870e4862bcf13f4aabfbaef41c38
FranklinJamesJones89 <fjones89@icloud.com>
2023-02-23 11:19:36 -0500

    Create browser side bar for rooms

--- dreams/views.py
+++ dreams/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -11,10 +12,52 @@ from .models import Room
 
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms': rooms}
+
+    topics = Topic.objects.all()
+    
+
+    context = {'rooms': rooms, 'topics': topics}
+    
     return render(request, 'dreams/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
     context = {'room': room}
+    
     return render(request, 'dreams/room.html', context)
+
+def create_room(request):
+    form = RoomForm()
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dreams:home') 
+
+    context = {'form': form}
+    return render(request, 'dreams/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance = room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance = room)
+        if form.is_valid():
+            form.save()
+            return redirect('dreams:home')
+    
+    context = {'form': form}
+
+    return render(request, 'dreams/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id = pk)
+    
+    if request.method == 'POST':
+        room.delete()
+        return redirect('dreams:home')
+
+    return render(request, 'dreams/delete.html', {'obj': room})
+
