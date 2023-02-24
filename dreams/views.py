02cd4ad64c2c4d1636252c93225a5551ca33aa20
FranklinJamesJones89 <fjones89@icloud.com>
2023-02-24 10:51:34 -0500

    Add login authentication for creating rooms

--- dreams/views.py
+++ dreams/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
-#rooms = [
-#    {'id': 1, 'name': 'Lets Learn Python!'},
-#    {'id': 2, 'name': 'Design with me'},
-#    {'id': 3, 'name': 'Frontend developers'},
-#]
-
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, 'User does not exist') 
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dreams:home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
     context = {}
     return render(request, 'dreams/login_register.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('dreams:home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -39,6 +58,7 @@ def room(request, pk):
     
     return render(request, 'dreams/room.html', context)
 
+@login_required(login_url='dreams:login')
 def create_room(request):
     form = RoomForm()
     
