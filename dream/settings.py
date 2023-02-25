189b5e0ca896d2308ef60045fc18bbfb729a8857
FranklinJamesJones89 <fjones89@icloud.com>
2023-02-25 12:54:09 -0500

    Start styling

--- dream/settings.py
+++ dream/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'dream.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'dreams/templates/dreams'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'dreams/static/'
+]
+
+#STATIC_ROOT =
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
